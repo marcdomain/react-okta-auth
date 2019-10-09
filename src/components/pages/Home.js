0cb48315f3caa9df511ed9ecd1880e053419c553
marcus chukwuoma <marcus2cu@gmail.com>
2019-10-09 16:07:28 +0100

    basic setup

---
+++ src/components/pages/Home.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { withAuth } from '@okta/okta-react';
+
+export default withAuth(class Home extends Component {
+  state = { authenticated: null };
+
+  checkAuthentication = async () => {
+    const authenticated = await this.props.auth.isAuthenticated();
+    if (authenticated !== this.state.authenticated) {
+      this.setState({ authenticated });
+    }
+  }
+
+  async componentDidMount() {
+    this.checkAuthentication();
+  }
+
+  async componentDidUpdate() {
+    this.checkAuthentication();
+  }
+
+  login = async () => {
+    this.props.auth.login('/');
+  }
+
+  logout = async () => {
+    this.props.auth.logout('/');
+  }
+
+  render() {
+    if (this.state.authenticated === null) return null;
+
+    const mainContent = this.state.authenticated ? (
+      <div>
+        <p className='lead'>You are logged in. <Link to='/staff'>Click here</Link> to visit your portal</p>
+        <button className='btn btn-light btn-lg' onClick={this.logout}>Logout</button>
+      </div>
+    ) : (
+      <div>
+        <p className='lead'>If you're a staff member, please get your credentials from your supervisor</p>
+        <button className='btn btn-dark btn-lg' onClick={this.login}>Login</button>
+      </div>
+    );
+
+    return (
+      <div className='jumbotron'>
+        <h1 className='display-4'>Acme staff portal</h1>
+        {mainContent}
+      </div>
+    );
+  }
+});
